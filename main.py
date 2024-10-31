39175beb9aa6e72733683572cbfb34b4b981077d
Chris-Bent <chriswjbent@gmail.com>
2024-10-31 19:07:01 +0000

    blank screen working.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill(black)
+        screen.fill((0, 0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
