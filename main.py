ee527108036a8dc61a0d1847c2ca83eb219457b1
Chris-Bent <chriswjbent@gmail.com>
2024-10-31 20:18:52 +0000

    Player sprite added. Won't work tho :(

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 clock = pygame.time.Clock()
 dt = 0
@@ -8,16 +10,18 @@ dt = 0
 def main(dt):
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill((0, 0, 0))
+        screen.fill(("black"))
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         deltaTime = clock.tick(60) / 1000.0
         dt += deltaTime
-
+        
 
 if __name__ == "__main__":
     main(dt)
